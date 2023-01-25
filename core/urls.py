50af6aed18cbd3ce61e66f59c277ce7c9b14d061
Abubakr306060 <haydarovabubakr306060@gmail.com>
2023-01-25 21:46:01 +0600

    first commit

--- core/urls.py
+++ core/urls.py
@@ -15,13 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import hello, index , about , contacts
+from posts.views import hello, IndexView , about , contacts , PostDetailView , PostCreateView , PostDeleteView , PostUpdateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/',hello, name='hello'),
-    path("", index, name="main-page"),
+    path("", IndexView.as_view(), name="main-page"),
     path("about/",about, name="about-page"),
     path("contacts/",contacts, name="contacts-page"),
+    path("post/<int:pk>/", PostDetailView.as_view(),name="post-detail" ),
+    path("post/create", PostCreateView.as_view(), name="post-create"),
+    path("post/delete/<int:pk>", PostDeleteView.as_view(), name="post-delete" ),
+    path("post/update/<int:pk>", PostUpdateView.as_view(), name="post_update")
+  
+
 ]
--- posts/models.py
+++ posts/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField
+    title = models.CharField(max_length=200, verbose_name="Загаловок")
+    content = models.TextField(verbose_name= "содержание")
+
 
     def __str__(self):
         return self.title
+class Meta:
+    verbose_name = "пост"
+    verbosssss
